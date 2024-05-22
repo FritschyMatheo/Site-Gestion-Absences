ad5f5b7f36a74269b85fefc373e46d2c6a303f9d
FritschyMatheo <matheo.fritschy13@gmail.com>
2024-05-22 20:09:46 +0200

    Fix

--- SAE23/SAE_GA/cours_views.py
+++ SAE23/SAE_GA/cours_views.py
@@ -7,7 +7,7 @@ def ajout(request):
         form = CoursForm(request)
         if form.is_valid():
             Cours = form.save()
-            return render(request,"SAE_GA/Cours/affiche.html",{"Cours" : Cours})
+            return render(request,"SAE_GA/Cours/traitement.html",{"Cours" : Cours})
         else:
             return render(request,"SAE_GA/Cours/ajout.html",{"form": form})
     else :
@@ -18,7 +18,7 @@ def traitement(request):
     lform = CoursForm(request.POST)
     if lform.is_valid():
         cours = lform.save()
-        return render(request, "SAE_GA/Cours/affiche.html", {"cours" : cours})
+        return render(request, "SAE_GA/Cours/traitement.html", {"cours" : cours})
     else:
         return render(request, "SAE_GA/Cours/ajout.html", {"form" : lform})
     
@@ -30,3 +30,18 @@ def update(request, id):
     cours = models.Cours.objects.get( pk = id)
     form = CoursForm(cours.__dict__)
     return render(request, "SAE_GA/Cours/ajout.html", {"form" : form, "id": id})
+
+def traitementupdate(request, id):
+    lform = CoursForm(request.POST)
+    if lform.is_valid():
+        cours = lform.save(commit=False)
+        cours.id = id
+        cours.save()
+        return HttpResponseRedirect("/onepiece/equipages")
+    else:
+        return render(request, "onepiece/equipages/creationequipage.html", {"form" : lform, "id": id})
+    
+def delete(request, id):
+    cours = models.Cours.objects.get( pk = id)
+    cours.delete()
+    return HttpResponseRedirect("/onepiece/equipages")
