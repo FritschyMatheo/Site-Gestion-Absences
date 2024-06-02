bbfd852b1dfa2ce0c49a04f9d8e5402adb775cff
Mathéo <148585740+FritschyMatheo@users.noreply.github.com>
2024-06-02 12:40:15 +0200

    name des groupes

--- SAE23/SAE_GA/models.py
+++ SAE23/SAE_GA/models.py
@@ -6,7 +6,7 @@ class Groupe(models.Model):
     nom = models.CharField(max_length=100)
 
     def __str__(self):
-        chaine = f"Nom du groupe : {self.nom}"
+        chaine = f"{self.nom}"
         return chaine
 
 class Cours(models.Model):
