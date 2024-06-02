5f8b7a2efa58f1417042bd8fe57e8ea464a7f997
Mathéo <148585740+FritschyMatheo@users.noreply.github.com>
2024-06-02 20:32:48 +0200

    Importation CSV ok

--- SAE23/SAE_GA/models.py
+++ SAE23/SAE_GA/models.py
@@ -19,7 +19,7 @@ class Cours(models.Model):
     groupe=models.ForeignKey("groupe", on_delete=models.CASCADE, default=None)
 
     def __str__(self):
-        chaine = f"Le cours de {self.titre} ayant lieu le {self.date} est encadré par {self.enseignant}. Il dure {self.duree}."
+        chaine = f"{self.titre}"
         return chaine
 
 class Absence(models.Model):
@@ -32,9 +32,9 @@ class Absence(models.Model):
 
     def __str__(self):
         if self.accepte == True:
-            chaine = f"L'étudiant {self.etudiant} n'était pas présent au cours de {self.cours}. Absence acceptée."
+            chaine = f"Absence de {self.etudiant}  au cours de {self.cours}. Absence acceptée."
         else :
-            chaine = f"L'étudiant {self.etudiant} n'était pas présent au cours de {self.cours}. Absence refusée car justification non valable {self.justification}"
+            chaine = f"Absence de {self.etudiant}  au cours de {self.cours}. Absence refusée car justification non valable : {self.justification}"
         return chaine
 
 class Enseignant(models.Model):
@@ -54,5 +54,5 @@ class Etudiant(models.Model):
     groupe = models.ForeignKey("groupe", on_delete=models.CASCADE, default=None)
     photo = models.ImageField(null=True)
     def __str__(self):
-        chaine = f"{self.nom.upper()} {self.prenom}, groupe : {self.groupe}"
+        chaine = f"{self.nom.upper()} {self.prenom}"
         return chaine
